51213aa2c5a872eac47a828f2302bf413b00192c
cierra00 <cierraray2@gmail.com>
2024-10-06 00:56:22 -0400

    push checkpoint

--- server.js
+++ server.js
@@ -14,8 +14,5 @@ app.use(logger);
 app.use(cors());
 app.use(express.json());
 
-app.use('/', require('./routes/root'));
-app.use('/register', require('./routes/register'));
-app.use('/auth', require('./routes/auth'));
-app.use('/refresh', require('./routes/refresh'));
-app.use('/logout', require('./routes/logout'));
+app.use('/', require('./routes/route'));
+// app.use('/register', require('./routes/register'));
